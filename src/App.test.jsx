e9eb13b4adcf14d89d2e173c8b08f88d57dee158
iliass <ichemrah@omp.com>
2026-03-08 23:36:47 +0100

    add failed test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
-// import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event'
 import App from './App'
 
 describe('App', () => {
@@ -15,11 +15,11 @@ describe('App', () => {
   })
 
 
-//   it('increments count on button click', async () => {
-//     const user = userEvent.setup()
-//     render(<App />)
-//     const button = screen.getByRole('button', { name: /count is 0/i })
-//     await user.click(button)
-//     expect(screen.getByRole('button', { name: /count is 1/i })).toBeInTheDocument()
-//   })
+  it('increments count on button click', async () => {
+    const user = userEvent.setup()
+    render(<App />)
+    const button = screen.getByRole('button', { name: /count is 0/i })
+    await user.click(button)
+    expect(screen.getByRole('button', { name: /count is 1/i })).toBeInTheDocument()
+  })
 })
